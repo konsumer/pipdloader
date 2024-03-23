e47a25f684b166524ae597ede8afc87505279fb0
David Konsumer <konsumer@jetboystudio.com>
2024-03-22 19:16:47 -0700

    workign on test program

--- src/test_hardware.c
+++ src/test_hardware.c
@@ -1,7 +1,5 @@
 #include <wiringPi.h>
-
-// for oled:
-// https://github.com/iliapenev/ssd1306_i2c/tree/master
+#include "ssd1306_i2c.h"
 
 // these are pins on audio-hat
 #define PIN_BUTTON 23
@@ -9,8 +7,29 @@
 
 int main(int argc, char* argv[]) {
   wiringPiSetupGpio();
+
   pinMode(PIN_BUTTON, INPUT);
   pinMode(PIN_LED, OUTPUT);
+
+  ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
+
+  ssd1306_display();  // Adafruit logo is visible
+  ssd1306_clearDisplay();
+  delay(5000);
+
+  char* text = "This is demo for SSD1306 i2c driver for Raspberry Pi";
+  ssd1306_drawString(text);
+  ssd1306_display();
+  delay(5000);
+
+  ssd1306_dim(1);
+  ssd1306_startscrollright(00, 0xFF);
+  delay(5000);
+
+  ssd1306_clearDisplay();
+  ssd1306_fillRect(10, 10, 50, 20, WHITE);
+  ssd1306_fillRect(80, 10, 130, 50, WHITE);
+  ssd1306_display();
 
   // pullUpDnControl(PIN_BUTTON, PUD_DOWN);
 
