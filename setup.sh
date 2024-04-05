1d47d6c95bbd9486ef731cc1cb1a687bc700578b
David Konsumer <konsumer@jetboystudio.com>
2024-04-04 17:51:04 -0700

    needs swig

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y git build-essential python3-pip python3-setuptools python3-pygame python3-pil
-sudo pip3 install --break-system-packages python-osc Adafruit-Blinka adafruit-circuitpython-ssd1306
+set -e
+
+apt-get install -y git build-essential python3-pip python3-setuptools python3-pygame python3-pil swig
+pip3 install --break-system-packages python-osc Adafruit-Blinka adafruit-circuitpython-ssd1306
 
 git clone --recursive https://github.com/libpd/libpd.git /tmp/libpd
 cd /tmp/libpd/python
 make
-sudo make install
+make install
